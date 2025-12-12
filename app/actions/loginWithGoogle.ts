4ceccfd910247ca565a4453c3cb27c20b5074898
omer mohamed <damouramory@gmail.com>
2025-12-12 21:15:24 +0200

    add base url

--- app/actions/loginWithGoogle.ts
+++ app/actions/loginWithGoogle.ts
@@ -3,6 +3,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export async function googleLoginAction() {
+  const baseUrl = "https://heal2-dashboard.vercel.app";
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -11,7 +12,7 @@ export async function googleLoginAction() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`,
+      redirectTo: `${baseUrl}/api/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
